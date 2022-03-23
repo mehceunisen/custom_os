a5d378abcd971a4103b9d63e1d86d7244217ffc8
MehceUnisen <mehceu@gmail.com>
2022-03-24 00:13:19 +0300

    added disk reading for bootloader

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,22 +1,25 @@
-mov ah, 0x0e
-; defining the offset value in to the extra segment register
-mov bx, 0x7c0 ; 0x7c00 doesn't work, i guess it is something related with register size
-mov es, bx
+[org 0x7c00]
+mov bp, 0x8000
+mov sp, bp
 
-mov al, [es:str_var]
-int 0x10
+mov bx, 0x9000
+mov dh, 2 ; read two sectors from initial sector we specified in ah reg
+call load_disk
 
-mov ds, bx
-mov al, [str_var]
 
-int 0x10
+mov dx, [0x9000]
+call print_hex
 
 
 jmp $
 
-str_var:
-    db 'a'
+%include "disk_read.asm"
+%include "print.asm"
+%include "print_hex.asm"
 
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
+
+times 256 dw 0x3131; sector 1
+times 256 dw 0xfa00 ; sector 2
--- src/bootloader/print.asm
+++ src/bootloader/print.asm
@@ -1,12 +1,17 @@
-mov al, [bx]
-
 print:
+    mov ah, 0x0e
     cmp al, 0
     je exit
     int 0x10
     inc bx
     mov al, [bx]
     jmp print
+
+print_one_char:
+    mov ah, 0x0e
+    mov al, [bx]
+    int 0x10
+    ret
 
 
 exit:
